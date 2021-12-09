c6b52011b56d7e77c2fd544330ecf78a06ccf4e9
Melih Sami Özaydın <30636062+melihozaydin@users.noreply.github.com>
2021-12-09 22:10:25 +0300

    hog extraction

--- BM5113 - Bilgisayarla Görme/hw2/src/object-detector/extract-features.py
+++ BM5113 - Bilgisayarla Görme/hw2/src/object-detector/extract-features.py
@@ -3,6 +3,7 @@ from skimage.feature import local_binary_pattern
 from skimage.feature import hog
 from skimage.io import imread
 from sklearn.externals import joblib
+
 # To read file names
 import argparse as ap
 import glob
@@ -13,17 +14,20 @@ from config import *
 if __name__ == "__main__":
     # Argument Parser
     parser = ap.ArgumentParser()
-    parser.add_argument('-p', "--pospath", help="Path to positive images",
-            required=True)
-    parser.add_argument('-n', "--negpath", help="Path to negative images",
-            required=True)
-    parser.add_argument('-d', "--descriptor", help="Descriptor to be used -- HOG",
-            default="HOG")
+    parser.add_argument(
+        "-p", "--pospath", help="Path to positive images", required=True
+    )
+    parser.add_argument(
+        "-n", "--negpath", help="Path to negative images", required=True
+    )
+    parser.add_argument(
+        "-d", "--descriptor", help="Descriptor to be used -- HOG", default="HOG"
+    )
     args = vars(parser.parse_args())
 
     pos_im_path = args["pospath"]
     neg_im_path = args["negpath"]
-	
+
     des_type = args["descriptor"]
 
     # If feature directories don't exist, create them
@@ -37,14 +41,16 @@ if __name__ == "__main__":
     print("Calculating the descriptors for the positive samples and saving them")
     matches = []
     for root, dirnames, filenames in os.walk(pos_im_path):
-        for filename in fnmatch.filter(filenames, '*.jpg'):
+        for filename in fnmatch.filter(filenames, "*.jpg"):
             matches.append(os.path.join(root, filename))
 
-    #for im_path in glob.glob(os.path.join(pos_im_path, "*")):
+    # for im_path in glob.glob(os.path.join(pos_im_path, "*")):
     for im_path in matches:
         im = imread(im_path, as_grey=True)
         if des_type == "HOG":
-            fd = hog(im, orientations, pixels_per_cell, cells_per_block, visualize, normalize)
+            fd = hog(
+                im, orientations, pixels_per_cell, cells_per_block, visualize, normalize
+            )
         fd_name = os.path.split(im_path)[1].split(".")[0] + ".feat"
         fd_path = os.path.join(pos_feat_ph, fd_name)
         joblib.dump(fd, fd_path)
@@ -54,10 +60,13 @@ if __name__ == "__main__":
     for im_path in glob.glob(os.path.join(neg_im_path, "*")):
         im = imread(im_path, as_grey=True)
         if des_type == "HOG":
-            fd = hog(im,  orientations, pixels_per_cell, cells_per_block, visualize, normalize)
+            fd = hog(
+                im, orientations, pixels_per_cell, cells_per_block, visualize, normalize
+            )
         fd_name = os.path.split(im_path)[1].split(".")[0] + ".feat"
         fd_path = os.path.join(neg_feat_ph, fd_name)
         joblib.dump(fd, fd_path)
+
     print("Negative features saved in {}").format(neg_feat_ph)
 
     print("Completed calculating features from training images")
